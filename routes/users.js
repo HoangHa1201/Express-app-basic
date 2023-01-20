d34dad2da83c33be8bca6e0c6249a0ad9ee53bde
HoangHa1201 <HaNH.B20DCCN213@stu.ptit.edu.vn>
2023-01-20 20:55:45 +0700

    update code

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('This is First Server NodeJS in Google Cloud Platform');
+  res.send('This is First Server NodeJS in Render Platform');
 });
 
 module.exports = router;
